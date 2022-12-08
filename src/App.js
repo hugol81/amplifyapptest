300e51b60ae2e7639cbcb8d9aedb49233ab7747a
hugol81 <hugoarce1981@gmail.com>
2022-12-07 18:18:30 -0600

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Front End Application.
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Interact with DB</p>
       </header>
     </div>
   );
